c9f058b2536395621a3247cfab34d43dda164e7b
Kobby <nkobby@gmail.com>
2018-06-07 19:51:38 +0100

    request query templates and alist; request answer to query

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,90 @@
 import React, { Component } from 'react';
-import { Button, Menu, Label, Input, Grid, Icon, Segment, Form, Header } from 'semantic-ui-react';
+import { Button, Grid, Icon, Segment, Form, Accordion, List, Header } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import '../home.css'
 
 class Home extends Component {
   constructor() {
     super();
-    this.state = { query: '', templates:[]  }
+    this.state = { query: '', templates:[], activeIndex: 1, alist:{}, alist_string:'', answer:{},}
     this.templates = [];
+    this.queryEx = [
+      "What will be the population of Ghana in 2025?",
+      "What is the capital of Germany?",
+      "What is the GDP of the country with the largest female unemployment in Africa?"                  
+    ]
+  }  
+
+  handleAccordionClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
   }
 
   handleChangeQuery(e) { 
     var queryStr = e.target.value
     this.setState({ query: queryStr})
-    if(queryStr[queryStr.length-1] == ' '){    
+    if(queryStr[queryStr.length-1] === ' ' || queryStr[queryStr.length-1] === '?'){    
       //generate the templates  
       fetch('http://localhost:5005/template/' + queryStr, {})
       .then(result => result.json())
-      .then(response => this.updateTemplates(response))
+      .then(response => this.updateAlistAndTemplates(response))
     }
   }
 
-  updateTemplates(t){
-    this.setState({templates:t});
+  handleRIFQuery(){
+    const { alist } = this.state
+    console.log(JSON.stringify(alist))
+    fetch('http://localhost:9876/query',{
+      method: 'POST',
+      body: JSON.stringify(alist)
+    })
+    .then(result => result.json())
+    .then(response => this.displayAnswer(response))
+  }
+
+  handleExItemClick = (e, listItemProps)=>{
+    const {children} = listItemProps
+    // console.log(children)
+    this.setState({query:children})
+  }
+
+  updateAlistAndTemplates(data){
+    console.log(data)
+    this.setState({templates:data['templates']});
+    this.setState({alist: data['alist'], alist_string: data['alist_string']})
+  }
+
+  displayAnswer(data){
+    console.log(data)
+    this.setState({answer: data})
   }
 
   render() {
-    var noBorderRadius = { borderRadius: '0px' }
+    var whiteBgStyle = { borderRadius: '0px', background: 'white', padding:'5px'}
+    const { activeIndex } = this.state
+    
+    const exlistItems = this.queryEx.map((ex, index)=>
+    <List.Item as='a' key={index} onClick={this.handleExItemClick}>{ex}</List.Item>
+    )
     
     return (
       <div>
         <Segment inverted color='teal' secondary style={{ borderRadius: '0px', margin: '0px' }}>
-          <div style={{ maxWidth: '800px', marginLeft: 'auto', marginRight:'auto' }}>
+          <div style={{ maxWidth: '800px' }}>
             <br />
             <Form>
-              <Form.Input
-                style={noBorderRadius} size='large'
+              <Form.Input className='no_input_focus'
+                value={this.state.query}
+                style={whiteBgStyle} size='large' transparent
                 placeholder='Type your query...'
                 action={
-                  <Button onClick={() => alert("Hello")} color='orange' icon labelPosition='left' style={noBorderRadius} size='large'>
+                  <Button onClick={this.handleRIFQuery.bind(this)} color='orange' icon labelPosition='left'
+                    style={{borderRadius: '0px', marginLeft:'8px'}} size='large'>
                     <Icon name='search' />Search
-                            </Button>
+                  </Button>
                 }
                 list='templates'
                 onChange={this.handleChangeQuery.bind(this)}
@@ -49,16 +94,45 @@ class Home extends Component {
                       return <option key= {key} value={e.value} />;
                 })}
               </datalist>
+              {/* query examples */}
+              <Accordion>
+                <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleAccordionClick}>
+                  <Icon name='dropdown' />
+                  Query Alist
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 0}>
+                  <Segment style={{borderRadius:'0px', paddingLeft: '20px', background:'#6FD5D0', border:'none', color:'white'}}>
+                    {this.state.alist_string}
+                  </Segment>
+                </Accordion.Content>
+
+                <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleAccordionClick}>
+                  <Icon name='dropdown' />
+                  See examples of queries
+                </Accordion.Title>
+                <Accordion.Content active={activeIndex === 1}>
+                  <Segment style={{borderRadius:'0px', paddingLeft: '20px', background:'#6FD5D0', border:'none'}}>
+                    <List link >
+                      {exlistItems}
+                    </List>
+                  </Segment>
+                </Accordion.Content>
+              </Accordion>
             </Form>
             <br />
           </div>
         </Segment>
 
-        <div style={{ marginLeft: 'auto', marginRight:'auto', paddingTop: '30px' }}>
-          {/* <Header>Answers</Header> */}
+        <div style={{ marginLeft: '15px', paddingTop: '30px' }}>
+          <Header>Answer</Header>
+          <Segment style={{borderRadius:'0px', paddingLeft: '20px', background:'#fff',
+           border:'none', color:'black', maxWidth:'800px'}}>
+              {JSON.stringify(this.state.answer)}
+          </Segment>
           <Grid stackable columns={2}>
 
           </Grid>
+
         </div>
       </div>
     );
--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Menu,Item, Icon, Dropdown} from 'semantic-ui-react';
+import {Menu,Item, Icon, Dropdown, Header, Label} from 'semantic-ui-react';
 import {Link, withRouter} from 'react-router-dom';
 
 class TopMenu extends Component {
@@ -10,7 +10,12 @@ class TopMenu extends Component {
     return (
         <Menu stackable color='teal' inverted  secondary style={{borderRadius: '0px', margin: '0px', minHeight:'60px'}}>
           <Menu.Item header>
-            <h3><Link to='/' style={{color:'#FFFFFF'}}>RIF-QA</Link></h3>
+            {/* <Link to='/' style={{color:'#FFFFFF'}}> */}
+              <Header as='h2'>FRANK
+                <Label color='red' size='mini'>alpha 0.0.1</Label>
+                <Header.Subheader>Functional Reasoning for Acquiring Novel Knowledge</Header.Subheader>
+              </Header>
+            {/* </Link> */}
           </Menu.Item>          
         </Menu>
     );
