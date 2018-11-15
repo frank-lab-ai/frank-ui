eda98cca3cf707117697b07c996b7f9170d0af29
Kobby <nkobby@gmail.com>
2018-11-15 14:48:00 +0000

    port change

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,7 +18,7 @@ class Home extends Component {
       "country in Europe with the lowest population in 2010"                  
     ]
     this.server_host = "34.242.204.151"; //;"localhost"
-    this.webui_api_endpoint = "http://" + this.server_host + ":5002";
+    this.webui_api_endpoint = "http://" + this.server_host + ":5005";
     this.frank_server_endpoint = "http://" + this.server_host + ":9876/query";
   }  
 
