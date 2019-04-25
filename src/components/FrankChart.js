ed91d060c63ed76bbff097e63eee3aca0d4ef38d
Kobby <nkobby@gmail.com>
2019-04-25 20:10:47 +0100

    added scaling of yaxis for numbers in billions

--- src/components/FrankChart.js
+++ src/components/FrankChart.js
@@ -6,7 +6,7 @@ import {
 class FrankChart extends Component {
   constructor() {
     super();
-    this.state = { data:[], prediction:[], curveData:[] }
+    this.state = { data:[], prediction:[], curveData:[], yLabel:'', yUnit:'' }
   }
 
   componentDidMount(){
@@ -17,42 +17,61 @@ class FrankChart extends Component {
     var data=[]
     var prediction=[]
     var curveData = []
+    var xArr = []
+    var isRescale = ''
+    var yLabel = 'y'
+    var yUnit = ''
     try{
+      var scale = 1000000000
       var fnPlot = JSON.parse(this.props.alist.fp)
-      data = fnPlot.data.map(d=> {return {x:d[0], y:d[1]} } ) 
+      isRescale = fnPlot.prediction[1] > scale;
+      data = fnPlot.data.map(d=> {
+        xArr.push(d[0])
+        var y = d[1]
+        if(isRescale) y = y/scale
+        return {x:d[0],y} 
+      }) 
+      xArr.push(fnPlot.prediction[0]) //add the prediction x value to the regression curve
       prediction = [{x:fnPlot.prediction[0], y:fnPlot.prediction[1]}]
-      curveData = fnPlot.data.map(d=> {
+      if(isRescale) {
+        prediction = [{x:fnPlot.prediction[0], y:fnPlot.prediction[1]/scale}]
+      }
+
+      var xWidenDiff = (Math.max(...xArr) - Math.min(...xArr))/3
+      xArr.push(...[Math.min(...xArr) - xWidenDiff, Math.max(...xArr)+xWidenDiff])      
+      curveData = xArr.map(x=> {
         var i
-        var x = d[0]
         var y = 0
         for (i=0; i < fnPlot.function.length; i++){
           y = y + (fnPlot.function[i] * Math.pow(x, i))
-        }
+        }         
+        if(isRescale) y = y/scale
         return {x:x, y:y}
-      })
-      curveData.push({x:fnPlot.prediction[0], y:fnPlot.prediction[1]})
+      })      
+      curveData.push({x:prediction[0].x, y:prediction[1].y})
     }
     catch(err){}
-    this.setState({data, prediction, curveData})
+    if(isRescale) {
+      yLabel = 'y (in billions)'
+      yUnit = 'e9'
+    }
+    this.setState({data, prediction, curveData, yLabel, yUnit})
   }
 
   render() {
     // const data = [
     //   { x: 100, y: 200, z: 200 },
     //   { x: 120, y: 100, z: 260 },
-    //   { x: 170, y: 300, z: 400 },
-    //   { x: 140, y: 250, z: 280 },
-    //   { x: 150, y: 400, z: 500 },
     //   { x: 110, y: 280, z: 200 },
     // ];
     const RenderNoShape = (props)=>{ 
       return null; 
      }
     return (
-      <div style={{marginLeft:'auto', marginRight:'auto'}}>
+      <div style={{marginLeft:'auto', marginRight:'auto', width:510}}>
       {this.state.data.length > 0 && this.state.prediction.length > 0 &&
         <ScatterChart
-          width={300}
+          width={500}
           height={300}
           margin={{
             top: 20, right: 20, bottom: 20, left: 20,
@@ -60,7 +79,7 @@ class FrankChart extends Component {
         >
           <CartesianGrid />
           <XAxis type="number" dataKey="x" name="x" unit="" domain={['auto', 'auto']}/>
-          <YAxis type="number" dataKey="y" name="y" unit="" domain={['auto', 'auto']}/>
+          <YAxis type="number" dataKey="y" name={this.state.ylabel} unit={this.state.yUnit} domain={['auto', 'auto']}/>
           <Tooltip cursor={{ strokeDasharray: '3 3' }} />
           <Scatter name="fn" data={this.state.curveData} line={{stroke: '#F26202', strokeWidth: 1}} lineType="fitting" shape={<RenderNoShape />}  />
           <Scatter name="data" data={this.state.data} fill="#8884d8" />         
