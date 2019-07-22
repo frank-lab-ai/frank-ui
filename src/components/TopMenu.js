d649cfd7225b5a3a3660934b8c96f0591c1545fb
Kobby <nkobby@gmail.com>
2019-07-22 21:42:04 +0100

    added FRANK logo

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Menu, Header, Label} from 'semantic-ui-react';
+import {Menu, Header, Label, Image} from 'semantic-ui-react';
 
 class TopMenu extends Component {
   render() {
@@ -8,7 +8,8 @@ class TopMenu extends Component {
                 style={{borderRadius: '0px', margin: '0px', minHeight:'60px', background:'#2D3142'}}>
           <Menu.Item header>
             {/* <Link to='/' style={{color:'#FFFFFF'}}> */}
-              <Header as='h1' style={{color:'#c1d2e1'}}>FRANK
+              <Header as='h1' style={{color:'#c1d2e1'}}>
+                <Image src={require('./../frank-logo.png')} centered style={{width: 90}}/>
                 <Label size='mini' style={{background:'#252937', color:'#7B8893'}}>v.0.1.1</Label>
                 <Header.Subheader  style={{color:'#9AAAB7'}}>Functional Reasoning Acquires New Knowledge</Header.Subheader>
               </Header>
