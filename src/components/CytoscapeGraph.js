272312669e4851029d6b97e09a128dc36bcbfafa
Kobby <nkobby@gmail.com>
2019-10-10 00:57:32 +0100

    enable hide/show successor nodes; activated graph  context menu commands

--- src/components/CytoscapeGraph.js
+++ src/components/CytoscapeGraph.js
@@ -81,60 +81,113 @@ let conf = {
     }
 };
 
-let menu_defaults = {
-    menuRadius: 50, // the radius of the circular menu in pixels
-    selector: 'node', // elements matching this Cytoscape.js selector will trigger cxtmenus
-    commands: [ // an array of commands to list in the menu or a function that returns the array
+// let menu_defaults = {
+//     menuRadius: 50, // the radius of the circular menu in pixels
+//     selector: 'node', // elements matching this Cytoscape.js selector will trigger cxtmenus
+//     commands: [ // an array of commands to list in the menu or a function that returns the array
       
-      { // example command
-        fillColor: 'rgba(61,65,78, 0.80)', // optional: custom background color for item
-        content: "<img src='images/solution.png'  height='20px' width='20px' />", // html/text content to be displayed in the menu
-        // contentStyle: {}, // css key:value pairs to set the command's css in js if you want
-        // select: function(ele){ // a function to execute when the command is selected
-        //   console.log( ele.id() ) // `ele` holds the reference to the active element
-        // },
-        enabled: true // whether the command is selectable
-      },
-      { // example command
-        fillColor: 'rgba(61,65,78, 0.80))', // optional: custom background color for item
-        content: "<img src='images/network.png' height='20px' width='20px' />", // html/text content to be displayed in the menu
-        // contentStyle: {}, // css key:value pairs to set the command's css in js if you want
-        // select: function(ele){ // a function to execute when the command is selected
-        //   console.log( ele.id() ) // `ele` holds the reference to the active element
-        // },
-        enabled: true // whether the command is selectable
-      },
-      { // example command
-        fillColor: 'rgba(61,65,78, 0.80))', // optional: custom background color for item
-        content: "<img src='images/visibility.png'  height='20px' width='20px' />", // html/text content to be displayed in the menu
-        // contentStyle: {}, // css key:value pairs to set the command's css in js if you want
-        // select: function(ele){ // a function to execute when the command is selected
-        //   console.log( ele.id() ) // `ele` holds the reference to the active element
-        // },
-        enabled: true // whether the command is selectable
-      },
+//       { // example command
+//         fillColor: 'rgba(61,65,78, 0.80)', // optional: custom background color for item
+//         content: "<img src='images/solution.png'  height='20px' width='20px' />", // html/text content to be displayed in the menu
+//         // contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+//         // select: function(ele){ // a function to execute when the command is selected
+//         //   console.log( ele.id() ) // `ele` holds the reference to the active element
+//         // },
+//         enabled: true // whether the command is selectable
+//       },
+//       { // show children
+//         fillColor: 'rgba(61,65,78, 0.80))', // optional: custom background color for item
+//         content: "<img src='images/network.png' height='20px' width='20px' />", // html/text content to be displayed in the menu
+//         // contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+//         // select: function(ele){ // a function to execute when the command is selected
+//         //   console.log( ele.id() ) // `ele` holds the reference to the active element
+//         // },
+//         enabled: true // whether the command is selectable
+//       },
+//       { // hide node
+//         fillColor: 'rgba(61,65,78, 0.80))', // optional: custom background color for item
+//         content: "<img src='images/visibility.png'  height='20px' width='20px' />", // html/text content to be displayed in the menu
+//         // contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+//         select: function(ele){ // a function to execute when the command is selected
+//           console.log( ele.id() ) // `ele` holds the reference to the active element
+//         },
+//         enabled: true // whether the command is selectable
+//       },
       
-    ], // function( ele ){ return [ /*...*/ ] }, // a function that returns commands or a promise of commands
-    fillColor: 'rgba(61,65,78, 0.80)', // the background colour of the menu
-    activeFillColor: 'rgba(0,153,153, 0.80)', // the colour used to indicate the selected command
-    activePadding: 15, // additional size in pixels for the active command
-    indicatorSize: 15, // the size in pixels of the pointer to the active command
-    separatorWidth: 2, // the empty spacing in pixels between successive commands
-    spotlightPadding: 2, // extra spacing in pixels between the element and the spotlight
-    minSpotlightRadius: 10, // the minimum radius in pixels of the spotlight
-    maxSpotlightRadius: 25, // the maximum radius in pixels of the spotlight
-    openMenuEvents: 'cxttapstart taphold', // space-separated cytoscape events that will open the menu; only `cxttapstart` and/or `taphold` work here
-    itemColor: 'white', // the colour of text in the command's content
-    itemTextShadowColor: 'transparent', // the text shadow colour of the command's content
-    zIndex: 9999, // the z-index of the ui div
-    atMouse: false // draw menu at mouse position
-  };
+//     ], // function( ele ){ return [ /*...*/ ] }, // a function that returns commands or a promise of commands
+//     fillColor: 'rgba(61,65,78, 0.80)', // the background colour of the menu
+//     activeFillColor: 'rgba(0,153,153, 0.80)', // the colour used to indicate the selected command
+//     activePadding: 15, // additional size in pixels for the active command
+//     indicatorSize: 15, // the size in pixels of the pointer to the active command
+//     separatorWidth: 2, // the empty spacing in pixels between successive commands
+//     spotlightPadding: 2, // extra spacing in pixels between the element and the spotlight
+//     minSpotlightRadius: 10, // the minimum radius in pixels of the spotlight
+//     maxSpotlightRadius: 25, // the maximum radius in pixels of the spotlight
+//     openMenuEvents: 'cxttapstart taphold', // space-separated cytoscape events that will open the menu; only `cxttapstart` and/or `taphold` work here
+//     itemColor: 'white', // the colour of text in the command's content
+//     itemTextShadowColor: 'transparent', // the text shadow colour of the command's content
+//     zIndex: 9999, // the z-index of the ui div
+//     atMouse: false // draw menu at mouse position
+//   };
 
 class CytoscapeGraph extends Component {
     constructor(props) {
     super(props);
     this.state = { cy: {} }
     }
+
+    menu_defaults = {
+        menuRadius: 50, // the radius of the circular menu in pixels
+        selector: 'node', // elements matching this Cytoscape.js selector will trigger cxtmenus
+        commands: [ // an array of commands to list in the menu or a function that returns the array
+          
+          { // example command
+            fillColor: 'rgba(61,65,78, 0.80)', // optional: custom background color for item
+            content: "<img src='images/solution.png'  height='20px' width='20px' />", // html/text content to be displayed in the menu
+            // contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+            select: (ele)=>{ // a function to execute when the command is selected
+                // console.log( ele.id() ) // `ele` holds the reference to the active element
+                this.props.handleNodeClick(ele.id());
+            },
+            enabled: true // whether the command is selectable
+          },
+          { // show children
+            fillColor: 'rgba(61,65,78, 0.80))', // optional: custom background color for item
+            content: "<img src='images/network.png' height='20px' width='20px' />", // html/text content to be displayed in the menu
+            // contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+            select: (ele)=>{ // a function to execute when the command is selected
+              //console.log( ele.id() ) // `ele` holds the reference to the active element
+              if (ele.successors().targets()[0] !== undefined && ele.successors().targets()[0].style("display") == "none")
+                ele.successors().targets().style("display", "element");
+            },
+            enabled: true // whether the command is selectable
+          },
+          { // hide node
+            fillColor: 'rgba(61,65,78, 0.80))', // optional: custom background color for item
+            content: "<img src='images/visibility.png'  height='20px' width='20px' />", // html/text content to be displayed in the menu
+            // contentStyle: {}, // css key:value pairs to set the command's css in js if you want
+            select: (ele)=>{ // a function to execute when the command is selected
+              console.log( ele.id() ); // `ele` holds the reference to the active element
+              ele.successors().targets().style("display", "none");
+            },
+            enabled: true // whether the command is selectable
+          },
+          
+        ], // function( ele ){ return [ /*...*/ ] }, // a function that returns commands or a promise of commands
+        fillColor: 'rgba(61,65,78, 0.80)', // the background colour of the menu
+        activeFillColor: 'rgba(0,153,153, 0.80)', // the colour used to indicate the selected command
+        activePadding: 15, // additional size in pixels for the active command
+        indicatorSize: 15, // the size in pixels of the pointer to the active command
+        separatorWidth: 2, // the empty spacing in pixels between successive commands
+        spotlightPadding: 2, // extra spacing in pixels between the element and the spotlight
+        minSpotlightRadius: 10, // the minimum radius in pixels of the spotlight
+        maxSpotlightRadius: 25, // the maximum radius in pixels of the spotlight
+        openMenuEvents: 'cxttapstart taphold', // space-separated cytoscape events that will open the menu; only `cxttapstart` and/or `taphold` work here
+        itemColor: 'white', // the colour of text in the command's content
+        itemTextShadowColor: 'transparent', // the text shadow colour of the command's content
+        zIndex: 9999, // the z-index of the ui div
+        atMouse: false // draw menu at mouse position
+      };
 
     componentDidMount() {
         this.updateGraph()
@@ -149,7 +202,7 @@ class CytoscapeGraph extends Component {
         conf.container = this.cyRef;
         conf.elements = this.props.data;
         const cy = cytoscape(conf);
-        const menu = cy.cxtmenu(menu_defaults)
+        const menu = cy.cxtmenu(this.menu_defaults)
         cy.center()
         cy.on('tap', 'node', (evt)=>{
             var nodeId = evt.target.id();
@@ -167,6 +220,16 @@ class CytoscapeGraph extends Component {
         //   });
 
         this.state = { cy };
+    }
+
+    hideChildren = (target)=>{
+        console.log(target)
+        target.successors().targets().style("display", "none");
+       
+        // else {
+        //     //hide the children nodes and edges recursively
+        //     target.successors().targets().style("display", "none");
+        //     }
     }
 
     componentWillReceiveProps(nextProps) {
